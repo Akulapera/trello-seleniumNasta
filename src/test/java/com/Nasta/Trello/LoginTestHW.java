c6765188ce16e9127068b0cbbee2791349c6fd57
Akulapera <porokhnia.anastasiya@gmail.com>
2020-01-05 18:28:17 +0200

    lesson 29.12 full done

--- src/test/java/com/Nasta/Trello/LoginTestHW.java
+++ src/test/java/com/Nasta/Trello/LoginTestHW.java
@@ -1,57 +1,44 @@
 package com.Nasta.Trello;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+public class LoginTestHW extends TestBase {
 
-public class LoginTestHW {
-    WebDriver wd;
-
-    @BeforeClass
-    public  void setUp(){
-        wd=new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wd.get("https://trello.com/");
-    }
-
-     @Test
-    public  void testLogin () throws InterruptedException {
-        // click login
-         clickLoginLink();
-
-     }
-
-    public void clickLoginLink() throws InterruptedException {
-        wd.findElement(By.cssSelector("[href='/login']")).click();
-        wd.findElement(By.id("user")).click();
-        wd.findElement(By.id("user")).clear();
-        wd.findElement(By.id("user")).sendKeys("porokhnia.anastasiya@gmail.com");
-        Thread.sleep(10000);
-
-        if(isElementPresent(By.id("password"))){
-            wd.findElement(By.id("password")).click();
-            wd.findElement(By.id("password")).clear();
-            wd.findElement(By.id("password")).sendKeys("495561na");
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (isAvatarPresentOnHeader()) {
+            logout();
         }
-        wd.findElement(By.id("login")).click();
-
-           if (isElementPresent(By.id("login-submit"))){
-               wd.findElement(By.id("login-submit")).click();
-
-               wd.findElement(By.id("password")).click();
-               wd.findElement(By.id("password")).clear();
-               wd.findElement(By.id("password")).sendKeys("495561na");
-
-               wd.findElement(By.id("login-submit")).click();
-           }
     }
 
-     public  boolean  isElementPresent(By locator)
-     {
-        return wd.findElements(locator).size()>0;
-     }
+    @Test(enabled = false)
+    public void testLogInWithAtlassianAcc() throws InterruptedException {
+        clickLoginLink();
+        fillLoginFormAtlassianAcc("porokhnia.anastasiya@gmail.com", "495561na");
+        pause(20000);
+        Assert.assertTrue
+                (isAvatarPresentOnHeader());
+    }
+
+    @Test(enabled = false)
+    public void negativeTestLogInWithAtlassianAcc() throws InterruptedException {
+        clickLoginLink();
+        fillLoginFormAtlassianAcc("roch", "");
+        pause(8000);
+        Assert.assertTrue(isErrorPresent());
+        Assert.assertTrue
+                (!isAvatarPresentOnHeader());
+    }
+
+    @Test
+    public void testLogInOldAcc() {
+        clickLoginLink();
+        fillLoginFormOldAcc("porokhnia.anastasiya@gmail.com", "495561na");
+        confirmLogin();
+        Assert.assertTrue
+                (isAvatarPresentOnHeader());
+    }
+
 }
--- src/test/java/com/Nasta/Trello/TestBase.java
+++ src/test/java/com/Nasta/Trello/TestBase.java
@@ -1,66 +1,118 @@
+
 package com.Nasta.Trello;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-
-import java.util.concurrent.TimeUnit;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import org.testng.annotations.AfterClass;
+        import org.testng.annotations.AfterSuite;
+        import org.testng.annotations.BeforeSuite;
+        import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
+   static  WebDriver wd;
+    @BeforeSuite
 
-    WebDriver wd;
-
-    @BeforeClass
-    public  void setUp(){
-        wd=new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    public void setUp() {
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         wd.get("https://trello.com/");
     }
 
-    public void login(String login, String password) throws InterruptedException {
-        clickCSS("[href='/login']");
-        type(By.id("user"), login);
-
-
-        if (wd.findElement(By.id ("password")).isDisplayed()) {
-            type(By.id("password"), password);
-        }
-        clickId(By.id ("login"));
-
-        if (isElementPresent(By.id ("login-submit"))) {
-            clickId(By.id ("login-submit"));
-            type(By.id ("password"), "495561na");
-            clickId(By.id ("login-submit"));
-        }
-
+    public  boolean isElementPresent(By locator){
+        return wd.findElements(locator).size()>0;
     }
 
-    private void clickCSS(String cssLocator) {
-        wd.findElement(By.cssSelector(cssLocator)).click();
+    @AfterSuite
+    public void tearDown(){
+        wd.quit();
     }
 
-    private void clickId(By locator) {
+    public void pause(int millis) throws InterruptedException {
+        Thread.sleep(millis);
+    }
 
+    public void fillLoginFormAtlassianAcc(String user, String pwd) throws InterruptedException {
+        type(By.id("user"), user);
+        pause(5000);
+        click(By.id("login"));
+        click(By.id("login-submit"));
+        type(By.id("password"), pwd);
+        click(By.id("login-submit"));
+    }
+
+    public void confirmLogin() {
+        click(By.id("login"));
+    }
+
+    public void click(By locator) {
         wd.findElement(locator).click();
     }
 
-    private void type(By locator, String text) {
-        clickId(locator);
+    public void fillLoginFormOldAcc(String userName, String password) {
+        type(By.id("user"), userName);
+        type(By.id("password"), password);
+    }
+
+    public void type(By locator, String text) {
+        click(locator);
         wd.findElement(locator).clear();
         wd.findElement(locator).sendKeys(text);
     }
 
-    public  boolean  isElementPresent(By locator)
-    {
-        return wd.findElements(locator).size()>0;
-     }
+    public void clickLoginLink(){
+        click(By.cssSelector("[href='/login']"));
 
-      @AfterClass
-    public  void tearDown (){
-        wd.quit();
-      }
+    }
+    public void loginWhithBothAccs() throws InterruptedException {
+        type(By.id("user"), "porokhnia.anastasiya@gmail.com");
+        Thread.sleep(10000);
+        if(wd.findElement(By.id("password")).isDisplayed()){
+            type(By.id("password"), "495561na");
+        }
+        click(By.id("login"));
+
+        if (isElementPresent(By.id("login-submit"))){
+            click(By.id("login-submit"));
+            type(By.id("password"), "12345.com");
+            click(By.id("login-submit"));
+        }
+    }
+
+    public boolean isAvatarPresentOnHeader() {
+        return isElementPresent
+                (By.cssSelector("[data-test-id='header-member-menu-button']"));
+    }
+
+    public void logout() {
+        clickOnAvatar();
+        clickLogoutButton();
+    }
+
+//    public void loginAtlassianAcc() throws InterruptedException {
+//        clickLoginLink();
+//        fillLoginFormAtlassianAcc("porokhnia.anastasiya@gmail.com", "495561na");
+//        pause(20000);
+//    }
+
+
+    public void testLogInOldAcc() throws InterruptedException {
+        clickLoginLink();
+        fillLoginFormOldAcc("porokhnia.anastasiya@gmail.com", "495561na");
+        confirmLogin();
+    }
+
+    public void clickLogoutButton() {
+        click(By.cssSelector("[data-test-id='header-member-menu-logout']"));
+    }
+
+    public void clickOnAvatar() {
+        click(By.cssSelector("[data-test-id='header-member-menu-button']"));
+    }
+
+    public boolean isErrorPresent() {
+        return isElementPresent(By.id("error"));
+    }
+
 }
-
